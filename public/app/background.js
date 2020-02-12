56d11410799c7d76dc8f17b4e31176ae95e78cf4
Leander Seige <leander@seige.name>
2020-02-12 03:29:30 +0100

    force cache

--- public/app/background.js
+++ public/app/background.js
@@ -1,5 +1,7 @@
 (function() {
+    var currentTabId = false;
     const tabStorage = {};
+    const cache = {};
     const networkFilters = {
         urls: [
             "<all_urls>"
@@ -9,7 +11,8 @@
     chrome.runtime.onMessage.addListener((msg, sender, response) => {
         switch (msg.type) {
             case 'popupInit':
-                response(tabStorage[msg.tabId]);
+                // response(tabStorage[msg.tabId]);
+                response(tabStorage[currentTabId]);
                 break;
             default:
                 response('unknown request');
@@ -17,15 +20,56 @@
         }
     });
 
-    function analyzeBody(body) {
-        if(!body.hasOwnProperty("@context"))
+    function initTab(tabId) {
+        tabStorage[tabId] = {
+            id: tabId,
+            requests: {},
+            iiif: {
+                manifests: {},
+                images: {},
+                collections: {}
+            },
+            registerTime: new Date().getTime()
+        };
+    }
+
+    function compileData(data,url) {
+        var tabId = currentTabId;
+        var iiif = analyzeBody(data,url);
+        if(!iiif)
+            return;
+        var item = {}
+        item.id = data['@id'];
+        item.url = url;
+        item.label = url; // data.label;
+        if(iiif.api=="presentation" && iiif.type=="manifest") {
+            item.thumb = data['sequences'][0]['canvases'][0]['images'][0]['resource']['service']['@id']+'/full/200,/0/default.jpg';
+        } else if (iiif.api=="image") {
+            item.thumb = data['@id']+'/full/200,/0/default.jpg';
+        } else {
+            item.thumb = "logo-small.png";
+        }
+        if(iiif.type=="manifest") {
+            tabStorage[tabId].iiif.manifests[item.id] = item;
+        } else {
+            tabStorage[tabId].iiif.images[item.id] = item;
+        }
+        updateIcon(tabId);
+    }
+
+    function analyzeBody(body,url) {
+        if(!body.hasOwnProperty("@context")) {
+            cache[url]=false;
             return(false);
+        }
 
         var ctx = body["@context"].split("/");
         // alert(JSON.stringify(ctx));
 
-        if(ctx[2]!=="iiif.io" || ctx[3]!=="api")
+        if(ctx[2]!=="iiif.io" || ctx[3]!=="api") {
+            cache[url]=false;
             return false;
+        }
 
         var iiif = {
             api: ctx[4].toLowerCase(),
@@ -51,11 +95,32 @@
     }
 
     chrome.webRequest.onHeadersReceived.addListener((details) => {
-        const {
-            tabId,
-            requestId
-        } = details;
+        // const {
+        //     tabId,
+        //     requestId
+        // } = details;
+
+        var tabId = currentTabId;
+        var requestId = details.requestId;
+
+        // alert(JSON.stringify(details));
+
+        // console.log("prechecking "+details.url);
+
+        if(tabId==chrome.tabs.TAB_ID_NONE) {
+            console.log(".TAB_ID_NONE");
+            return;
+        }
+
         if (!tabStorage.hasOwnProperty(tabId)) {
+            // console.log("discard(1) "+details.url);
+            // return;
+            console.log("init tab "+tabId);
+            initTab(tabId);
+        }
+
+        if (details.method!="GET") {
+            cache[details.url]=false;
             return;
         }
 
@@ -70,12 +135,14 @@
                 accepted = true;
             }
             if(item.name=="Content-Length" && item.value>1000000) {
+                // console.log("discard(2) "+details.url);
                 accepted = false;
                 return;
             }
         }
 
         if (accepted==false) {
+            cache[details.url]=false;
             return;
         }
 
@@ -89,15 +156,21 @@
     }, networkFilters, ["responseHeaders"]);
 
     chrome.webRequest.onCompleted.addListener((details) => {
-        const {
-            tabId,
-            requestId
-        } = details;
+        // const {
+        //     tabId,
+        //     requestId
+        // } = details;
+
+        var tabId = currentTabId;
+        var requestId = details.requestId;
+
         if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
             return;
         }
 
-        const request = tabStorage[tabId].requests[requestId];
+        console.log("checking "+details.url);
+
+        var request = tabStorage[tabId].requests[requestId];
 
         Object.assign(request, {
             endTime: details.timeStamp,
@@ -107,47 +180,36 @@
         });
 
         const {url, requestDuration, status, responseHeaders} = request;
-        fetch(url)
-            .then(res => res.json())
-            .then((data) => {
-                var iiif = analyzeBody(data);
-                if(!iiif)
-                    return;
-                var item = {}
-                item.id = data['@id'];
-                item.url = url;
-                item.label = url; // data.label;
-                if(iiif.api=="presentation") {
-                    item.thumb = data['sequences'][0]['canvases'][0]['images'][0]['resource']['service']['@id']+'/full/200,/0/default.jpg';
-                } else if (iiif.api=="image") {
-                    item.thumb = data['@id']+'/full/200,/0/default.jpg';
-                } else {
-                    item.thumb = "logo-small.png";
-                }
-                if(iiif.type=="manifest") {
-                    tabStorage[tabId].iiif.manifests[item.id] = item;
-                } else {
-                    tabStorage[tabId].iiif.images[item.id] = item;
-                }
-                updateIcon(tabId);
-            })
-        .catch(console.log)
-        console.log(tabStorage[tabId].requests[details.requestId]);
+
+        if(cache.hasOwnProperty(url)) {
+            console.log("CACHE HIT: "+url);
+            if(cache[url]) {
+                compileData(cache[url],url);
+            }
+        } else {
+            console.log("CACHE MISS: "+url);
+            fetch(url, {cache: "force-cache"})
+                .then(res => res.json())
+                .then((data) => {
+                    cache[url] = data;
+                    compileData(data,url);
+                })
+                .catch((error) => {
+                    cache[url]=false;
+                    console.error('Error:', error);
+                });
+        }
+
+        // console.log(tabStorage[tabId].requests[details.requestId]);
+
     }, networkFilters, ["responseHeaders"]);
 
     chrome.tabs.onActivated.addListener((tab) => {
+        // alert(JSON.stringify(tab));
         const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+        currentTabId = tabId;
         if (!tabStorage.hasOwnProperty(tabId)) {
-            tabStorage[tabId] = {
-                id: tabId,
-                requests: {},
-                iiif: {
-                    manifests: {},
-                    images: {},
-                    collections: {}
-                },
-                registerTime: new Date().getTime()
-            };
+            initTab(tabId);
         }
         updateIcon(tabId);
     });
